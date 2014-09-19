4b4ad112f109ec236cefea55accc7a90f5ca8237
Horacio Bertorello <syrii@msn.com>
2014-09-19 10:46:39 -0300

    Fix styles and behavior.

--- public/site.js
+++ public/site.js
@@ -1,32 +1,6 @@
-min_slide = 0;
-max_slide = document.getElementById('slides').children.length - 1;
+var prev = function() { window.scrollBy(0, -window.innerHeight); }
+var next = function() { window.scrollBy(0, window.innerHeight); }
 
-function slide() {
-  if (localStorage["slide"] == undefined) {
-    return 0;
-  } else {
-    return parseInt(localStorage["slide"]);
-  }
-}
-
-function mark(n) {
-  if (n < min_slide) n = min_slide;
-  if (n > max_slide) n = max_slide;
-
-  window.localStorage["slide"] = n;
-}
-
-function prev() {
-  mark(slide() - 1);
-  window.scrollTo(0, window.innerHeight * slide());
-}
-
-function next() {
-  mark(slide() + 1);
-  window.scrollTo(0, window.innerHeight * slide());
-}
-
-/* Change slides with the left and right arrow keys */
 document.onkeyup = function(e) {
   switch(e.which) {
     case 37: prev(); break;
@@ -34,7 +8,6 @@ document.onkeyup = function(e) {
   }
 }
 
-/* Ignore the up and down arrow keys */
 document.onkeydown = function(e) {
   switch(e.which) {
     case 38:
@@ -47,5 +20,5 @@ document.onclick = function(e) {
 }
 
 window.onload = function() {
-  scrollTo(0, window.innerHeight * slide());
+  scrollTo(0, 0);
 }
